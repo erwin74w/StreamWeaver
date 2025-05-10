5ba9c4835599d34aa39eb645fbad77fad87dd7fa
erwin74w <erwin74w@gmail.com>
2025-05-10 21:50:04 +0200

    Update TickerOverlay.js

--- js/overlays/TickerOverlay.js
+++ js/overlays/TickerOverlay.js
@@ -1,29 +1,37 @@
 // js/overlays/TickerOverlay.js
+import { DEBUG } from '../ably-config.js';
+
+const LOG_PREFIX = "[TickerOverlay]";
+const logger = {
+    log: (...args) => DEBUG && console.log(LOG_PREFIX, ...args),
+    error: (...args) => console.error(LOG_PREFIX, ...args),
+};
+
 export class TickerOverlay {
     constructor(containerId, wrapperId, textSpanId) {
         this.containerElement = document.getElementById(containerId);
         this.contentWrapperElement = document.getElementById(wrapperId);
         this.textSpanElement = document.getElementById(textSpanId);
 
-        if (!this.containerElement) console.error(`TickerOverlay: Element with ID '${containerId}' not found.`);
-        if (!this.contentWrapperElement) console.error(`TickerOverlay: Element with ID '${wrapperId}' not found.`);
-        if (!this.textSpanElement) console.error(`TickerOverlay: Element with ID '${textSpanId}' not found.`);
+        if (!this.containerElement) logger.error(`Element with ID '${containerId}' not found.`);
+        if (!this.contentWrapperElement) logger.error(`Element with ID '${wrapperId}' not found.`);
+        if (!this.textSpanElement) logger.error(`Element with ID '${textSpanId}' not found.`);
+        logger.log("TickerOverlay instance created.");
     }
 
     updateAndShow(data = {}) {
         if (this.containerElement && this.textSpanElement && this.contentWrapperElement) {
             const newText = data.text || "StreamWeaver Ticker Active...";
-            const trailingSpaces = " \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0 "; // For continuous scroll appearance
+            const trailingSpaces = " \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0 "; 
             this.textSpanElement.textContent = newText + trailingSpaces;
 
-            // Restart animation by removing and re-adding the class
             if (this.contentWrapperElement.classList.contains('animate')) {
                 this.contentWrapperElement.classList.remove('animate');
-                void this.contentWrapperElement.offsetWidth; // Force reflow to restart animation
+                void this.contentWrapperElement.offsetWidth; 
             }
             this.contentWrapperElement.classList.add('animate');
             this.containerElement.classList.add('show-ticker');
-            console.log("Ticker: SHOWN/UPDATED with text:", newText);
+            logger.log("SHOWN/UPDATED with text:", newText);
         }
     }
 
@@ -31,7 +39,7 @@ export class TickerOverlay {
         if (this.containerElement && this.contentWrapperElement) {
             this.containerElement.classList.remove('show-ticker');
             this.contentWrapperElement.classList.remove('animate');
-            console.log("Ticker: HIDDEN");
+            logger.log("HIDDEN");
         }
     }
 
